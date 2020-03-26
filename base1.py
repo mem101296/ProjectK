7277a45639f6f7212555f36e774b81e7be34a625
mem101296 <mem101296@protonmail.com>
2020-03-25 23:08:20 -0500

    Cleaned
    Removed unneeded code and added notes for tomorrow.

--- base1.py
+++ base1.py
@@ -1,6 +1,7 @@
 #This will eventually be unscripted
+#Infinite loop needs to be created for user input
 
-def answer(userq): #Function answer
+def answer(userq): #Function answer can be called at any point
 
     if userq == "How are you?":
         A_answer = "> I am fine. Thanks."
@@ -14,30 +15,36 @@ def answer(userq): #Function answer
         C_answer = "> We are programmed to cause no harm to live forms."
         return C_answer
 
+    elif userq == "Help":
+        D_answer = "> Heres is what I can do? \n\"How are you?\" \n\"You can't\" \n\"What are your morals\" \n\"Help\""
+        return D_answer
+
     else:
-        D_answer = "> ErRoR... \n> Eeerror \n> Sorry How can I help?"
-        return D_answer #returns the answer
+        Z_answer = "> ErRoR... \n> Eeerror \n> Sorry How can I help?"
+        return Z_answer #returns the answer
 
 
-print("> Welcome User") #I want this to search a database of welcoming sentences
+print("> Welcome User My Name Is Python") #I want this to search a database of welcoming sentences
 username = input("> What is your name? \n") #This will search for a way to ask a users name
 
 print("> Hello " + username + "! How can I help?")
 
-userq = input() #Records users question
-
-userqOld = userq #records users answer
-
-print(answer(userq)) #calls function answer
+#Create loop below
 
 userq = input() #Records users question
 
-if userqOld == userq: #Allows for different output
-    print(answer(userq)) #Calls answer function
+userqAnswer = userq #records users answer
 
-elif userq == AdmIput:
+print(answer(userq)) #calls function according to user answer
+
+userq = input() #Records users question
+
+"""if userqAnswer == userq: #Allows for different output
+    print(answer(userq)) #Calls answer function"""
+
+"""elif userq == AdmIput:
     print("Welcome to the Admin panel")
-    print("Here is the menu:")
+    print("Here is the menu:")"""
 
-else:
-    print("end of program")
+"""else:
+    print("end of program")"""
---
+++ base_new.py
@@ -0,0 +1,10 @@
+userq = input() #Records users question
+
+userqOld = userq #records users answer
+
+userq = input() #Records users question
+
+print("> Welcome User My Name Is Python") #I want this to search a database of welcoming sentences
+username = input("> What is your name? \n") #This will search for a way to ask a users name
+
+print("> Hello " + username + "! How can I help?")
