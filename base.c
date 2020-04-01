84d15e8cf99dd59f62226aa2565a10d77b12741c
mem101296 <mem101296@protonmail.com>
2020-03-31 22:51:11 -0500

    Added a code and compiled
    Added a loop to print start by each character. I need to fix the array and speed it prints

--- base.c
+++ base.c
@@ -1,6 +1,20 @@
 /*This will be used for C programming*/
 
 #include <stdio.h> //declares library to import.
+#include <math.h> //imports math library
   int main(void){
+    char start[]={'s','t','a','r','t','i','n','g'};
+    int i = 0;
+      //start == "Hello";
+      //for (i=0; i<6; i++){ //loops until i = 6
+        //printf("Hello\n"); // prints hello
 
+//}
+
+while (start[i] != '\0')
+  {
+      printf("%c\n", start[i]);
+      i++;
+  }
+return 0;
   }
